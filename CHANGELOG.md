43c5c6426c9f6aa01e8aacac3c9a81e1753333c0
colmexdev <github@colmex.mx>
2022-04-26 21:01:40 -0500

    Update for Rails 7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.3.1
+
+Support for Rails 7
+
 # v0.3.0
 
 Support for Ratonvirus 0.3.0.
--- lib/ratonvirus/clamby/version.rb
+++ lib/ratonvirus/clamby/version.rb
@@ -2,6 +2,6 @@
 
 module Ratonvirus
   module Clamby
-    VERSION = "0.3.0"
+    VERSION = "0.3.1"
   end
 end
